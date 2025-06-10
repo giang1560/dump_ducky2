17bef6adbb3328a75db147cbe1f38b52c90dab43
khanhbq2002 <khanhbq2002@g5.studio>
2025-06-10 18:09:07 +0700

    [Update] idk

--- MyServer/Middleware/obj/Debug/net8.0/win-x64/Middleware.AssemblyInfo.cs
+++ MyServer/Middleware/obj/Debug/net8.0/win-x64/Middleware.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Middleware")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae4ce1b1623a986db484e574e8bfa132429eb914")]
 [assembly: System.Reflection.AssemblyProductAttribute("Middleware")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Middleware")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
